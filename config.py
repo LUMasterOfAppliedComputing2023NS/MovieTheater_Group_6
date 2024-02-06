fb42f0d13d769b875de44090f76c21503e2d423e
yaaaana <139760218+yana0918@users.noreply.github.com>
2024-02-06 19:53:37 +1300

    revert pa connection back

--- config.py
+++ config.py
@@ -1,10 +1,22 @@
 
 
-db_user = "root"
-db_pass = "root123456"
-db_host = '127.0.0.1'
+db_user = "yana00918"
+db_pass = "somerandompassword"
+db_host = 'yana00918.mysql.pythonanywhere-services.com'
 db_port = "3306"
-db_name = "movietheater"
+db_name = "yana00918$movietheater2"
 
 SECRET_KEY = 'secret'
 
+'''
+db_user="yana00918"
+db_pass="somerandompassword"
+db_host='yana00918.mysql.pythonanywhere-services.com'
+db_port="3306"
+db_name="yana00918$movietheater2"
+
+mysql -h ${db_host} -u $db_user -P $db_port 'yana00918$movietheater2'
+mysql -h ${db_host} -u ${db_user} -P $db_port < sql/db_schema.sql
+mysql -h ${db_host} -u ${db_user} -P $db_port "${db_name}" < sql/data.sql
+mysql -h ${db_host} -u $db_user -P $db_port 'yana00918$movietheater2' < sql/data.sql 
+'''
