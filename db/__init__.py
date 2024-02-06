83cf456c9adbfb16d9804363493f15a3cd83c26c
yaaaana <139760218+yana0918@users.noreply.github.com>
2024-02-06 19:41:48 +1300

    remove __pycache__, revert db/__init__.py changes

--- db/__init__.py
+++ db/__init__.py
@@ -3,16 +3,32 @@ from mysql.connector import MySQLConnection
 
 import config
 
-conn = mysql.connector.connect(
-    user=config.db_user,
-    password=config.db_pass,
-    host=config.db_host,
-    database=config.db_name,
-    port=config.db_port,
-    autocommit=True,
-    buffered=True
-)
+conn = None
 
+def init_conn():
+    global conn
+    conn = mysql.connector.connect(
+            user=config.db_user,
+            password=config.db_pass,
+            host=config.db_host,
+            database=config.db_name,
+            port=config.db_port,
+            autocommit=True,
+            buffered=True
+        )
 
 def get_session():
+    global conn
+    if conn is None:
+        init_conn()
+    else:
+        try:
+            conn.ping(
+                reconnect=True,
+                attempts=3,
+                delay=5,
+            )
+        except mysql.connector.InterfaceError as err:
+          init_conn()
+
     return conn.cursor(), conn
